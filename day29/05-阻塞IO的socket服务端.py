597fcda9ba4804a616c6ba451ba10ce9b4c9ad4d
e <13501077019@163.com>
2019-12-24 17:24:31 +0800

    add dirs

---
+++ day29/05-阻塞IO的socket服务端.py
@@ -0,0 +1,32 @@
+'''
+    阻塞IO服务端
+'''
+
+import socket
+import time
+
+server = socket.socket()
+server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+
+server.bind(('127.0.0.1',8083))
+server.listen(5)
+print('卡哪儿了')
+server.setblocking(False)
+
+rlist = []
+
+while 1:
+    try:
+        conn,addr = server.accept()
+        print(addr)
+        rlist.append(conn)
+        print('来自%s:%s的链接请求' % (addr[0],addr[1]))
+    except BlockingIOError:
+        print('去买点药')
+
+    time.sleep(0.1)
+    print('rlist',rlist,len(rlist))
+
+    for con in rlist:
+        from_client_msg = con.recv(1024)
+        print(from_client_msg)
